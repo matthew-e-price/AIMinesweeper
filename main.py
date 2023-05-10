e58679ee5b52258b617174286ddb4cc2e87242ba
price <priceypersonal@hotmail.com>
2023-05-10 08:20:33 -0700

    Updated title and code format

--- main.py
+++ main.py
@@ -53,25 +53,37 @@ def show_menu_frame():
     menu_top_frame = Frame(menu_frame, bg="gray", width=settings.WIDTH, height=200)
     menu_bottom_frame = Frame(menu_frame, width=settings.WIDTH, height=500)
 
-    menu_play_btn = Button(menu_bottom_frame,
-                           width=10,
-                           height=1,
-                           bg="#36A4FF",
-                           fg="white",
-                           font=("", 30),
-                           command=lambda: goto_difficulty_frame(),
-                           text="Play"
-                           )
-    menu_ai_btn = Button(menu_bottom_frame,
-                         width=10,
-                         height=1,
-                         bg="#36A4FF",
-                         fg="white",
-                         font=("", 30),
-                         command=lambda: goto_ai_frame(),
-                         text="Test AI"
-                         )
+    menu_play_btn = Button(
+        menu_bottom_frame,
+        width=10,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 30),
+        command=lambda: goto_difficulty_frame(),
+        text="Play"
+    )
+    menu_ai_btn = Button(
+        menu_bottom_frame,
+        width=10,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 30),
+        command=lambda: goto_ai_frame(),
+        text="Test AI"
+    )
+    menu_title_lbl = Label(
+        menu_top_frame,
+        bg="gray",
+        fg="white",
+        font=("", 50),
+        text="Minesweeper"
+    )
 
+    menu_top_frame.grid_propagate(False)
+    menu_top_frame.grid_columnconfigure(0, weight=1)
+    menu_top_frame.grid_rowconfigure(0, weight=1)
     menu_bottom_frame.grid_propagate(False)
     menu_bottom_frame.grid_columnconfigure(0, weight=1)
     menu_bottom_frame.grid_rowconfigure(0, weight=1)
@@ -81,6 +93,7 @@ def show_menu_frame():
     menu_top_frame.pack()
     menu_bottom_frame.pack(pady=100)
 
+    menu_title_lbl.grid(column=0, row=0)
     menu_play_btn.grid(column=0, row=0)
     menu_ai_btn.grid(column=0, row=1)
 
@@ -90,34 +103,47 @@ def show_difficulty_frame():
     difficulty_top_frame = Frame(difficulty_frame, bg="gray", width=settings.WIDTH, height=200)
     difficulty_bottom_frame = Frame(difficulty_frame, width=settings.WIDTH, height=500)
 
-    difficulty_easy = Button(difficulty_bottom_frame,
-                             width=10,
-                             height=1,
-                             bg="#36A4FF",
-                             fg="white",
-                             font=("", 30),
-                             command=lambda: goto_minesweeper_frame(10),
-                             text="Easy"
-                             )
-    difficulty_medium = Button(difficulty_bottom_frame,
-                               width=10,
-                               height=1,
-                               bg="#36A4FF",
-                               fg="white",
-                               font=("", 30),
-                               command=lambda: goto_minesweeper_frame(15),
-                               text="Medium"
-                               )
-    difficulty_hard = Button(difficulty_bottom_frame,
-                             width=10,
-                             height=1,
-                             bg="#36A4FF",
-                             fg="white",
-                             font=("", 30),
-                             command=lambda: goto_minesweeper_frame(20),
-                             text="Hard"
-                             )
+    difficulty_easy = Button(
+        difficulty_bottom_frame,
+        width=10,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 30),
+        command=lambda: goto_minesweeper_frame(10),
+        text="Easy"
+    )
+    difficulty_medium = Button(
+        difficulty_bottom_frame,
+        width=10,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 30),
+        command=lambda: goto_minesweeper_frame(15),
+        text="Medium"
+    )
+    difficulty_hard = Button(
+        difficulty_bottom_frame,
+        width=10,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 30),
+        command=lambda: goto_minesweeper_frame(20),
+        text="Hard"
+    )
+    difficulty_title_lbl = Label(
+        difficulty_top_frame,
+        bg="gray",
+        fg="white",
+        font=("", 50),
+        text="Minesweeper"
+    )
 
+    difficulty_top_frame.grid_propagate(False)
+    difficulty_top_frame.grid_columnconfigure(0, weight=1)
+    difficulty_top_frame.grid_rowconfigure(0, weight=1)
     difficulty_bottom_frame.grid_propagate(False)
     difficulty_bottom_frame.grid_columnconfigure(0, weight=1)
     difficulty_bottom_frame.grid_rowconfigure(0, weight=1)
@@ -128,6 +154,7 @@ def show_difficulty_frame():
     difficulty_top_frame.pack()
     difficulty_bottom_frame.pack(pady=100)
 
+    difficulty_title_lbl.grid(column=0, row=0)
     difficulty_easy.grid(column=0, row=0)
     difficulty_medium.grid(column=0, row=1)
     difficulty_hard.grid(column=0, row=2)
@@ -166,24 +193,26 @@ def show_ai_frame():
     create_board_ui(size=10, location=minesweeper_bottom_frame)
 
     Cell.create_cell_count_label(minesweeper_top_frame)
-    ai_step_btn = Button(minesweeper_top_frame,
-                         width=12,
-                         height=1,
-                         bg="#36A4FF",
-                         fg="white",
-                         font=("", 20),
-                         command=lambda: ai_step(agent, ai_step_btn),
-                         text="Take next step"
-                         )
-    ai_quit_btn = Button(minesweeper_top_frame,
-                         width=12,
-                         height=1,
-                         bg="#36A4FF",
-                         fg="white",
-                         font=("", 20),
-                         command=lambda: goto_menu_frame(),
-                         text="Return to menu"
-                         )
+    ai_step_btn = Button(
+        minesweeper_top_frame,
+        width=12,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 20),
+        command=lambda: ai_step(agent, ai_step_btn),
+        text="Take next step"
+    )
+    ai_quit_btn = Button(
+        minesweeper_top_frame,
+        width=12,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 20),
+        command=lambda: goto_menu_frame(),
+        text="Return to menu"
+    )
 
     Cell.cell_count_label_object.grid(column=0, row=0)
     ai_step_btn.grid(column=1, row=0)
@@ -228,15 +257,16 @@ def show_minesweeper_frame(size):
     create_board_ui(size=size, location=minesweeper_bottom_frame)
 
     Cell.create_cell_count_label(minesweeper_top_frame)
-    minesweeper_quit_btn = Button(minesweeper_top_frame,
-                                  width=12,
-                                  height=1,
-                                  bg="#36A4FF",
-                                  fg="white",
-                                  font=("", 20),
-                                  command=lambda: goto_menu_frame(),
-                                  text="Return to home"
-                                  )
+    minesweeper_quit_btn = Button(
+        minesweeper_top_frame,
+        width=12,
+        height=1,
+        bg="#36A4FF",
+        fg="white",
+        font=("", 20),
+        command=lambda: goto_menu_frame(),
+        text="Return to home"
+    )
 
     Cell.cell_count_label_object.grid(column=0, row=0)
     minesweeper_quit_btn.grid(column=1, row=0)
